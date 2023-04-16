3722e5f53b621f4439507e2f920ab6979ecf18f3
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-04-15 22:23:31 -0700

    Update __init__.py

--- ratslam/__init__.py
+++ ratslam/__init__.py
@@ -39,7 +39,7 @@ the use of OpenCV [4]_.
 .. [4] http://opencv.org/
 '''
 
-import numpy as cp
+import cupy as np
 from ratslam._globals import *
 from ratslam.visual_odometry import VisualOdometry
 from ratslam.view_cells import ViewCells
